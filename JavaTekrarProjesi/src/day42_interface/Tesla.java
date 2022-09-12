d0bd7399dd51a15f6dbc66b360990a24357b1656
coder1973 <bulentozturk2019@outlook.com>
2022-09-12 15:30:23 +0200

    Update

--- JavaTekrarProjesi/src/day42_interface/Tesla.java
+++ JavaTekrarProjesi/src/day42_interface/Tesla.java
@@ -1,5 +1,20 @@
 package day42_interface;
 
-public class Tesla {
+public class Tesla extends ElectricCar{
 
+	public Tesla(String model, double price, String color) {
+		super(model, price, color);
+	}
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void charge() {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void drive() {
+		// TODO Auto-generated method stub
+	}
 }
