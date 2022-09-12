94cfeb3f9529dd8c0e6af44f2b446373da1e005d
coder1973 <bulentozturk2019@outlook.com>
2022-09-12 15:23:32 +0200

    Update

--- JavaTekrarProjesi/src/day42_interface/TeslaTruck.java
+++ JavaTekrarProjesi/src/day42_interface/TeslaTruck.java
@@ -2,7 +2,7 @@ package day42_interface;
 
 public class TeslaTruck extends TeslaSemi{
 
-	public TeslaTruck(String model, double price, String color, double engineSize) { //concrete class
+	public TeslaTruck(String model, double price, String color, double engineSize) { 
 		super(model, price, color, engineSize);
 		
 	}
